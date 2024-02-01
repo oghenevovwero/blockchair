a0892941c261877aa70d3257c4ab8002e5c7b51d
tobore oghenevovwerho <145409454+oghenevovwero@users.noreply.github.com>
2024-02-01 13:52:01 +0100

    Update padded.tsx

--- components/wrappers/padded.tsx
+++ components/wrappers/padded.tsx
@@ -4,7 +4,7 @@ const PaddedWrapper: React.FC<{ children: ReactNode; className?: string }> = ({
   children,
   className = "",
 }) => {
-  return <div className={`px-2 2xl:px-[155px] ${className}`}>{children}</div>;
+  return <div className={`px-[14px] 2xl:px-[155px] ${className}`}>{children}</div>;
 };
 
 export default PaddedWrapper;
