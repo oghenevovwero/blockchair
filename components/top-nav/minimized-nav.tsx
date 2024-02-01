c0a43ac792fcbd270382ac0597f2aa6386176a25
tobore oghenevovwerho <145409454+oghenevovwero@users.noreply.github.com>
2024-02-01 12:50:37 +0100

    Update minimized-nav.tsx

--- components/top-nav/minimized-nav.tsx
+++ components/top-nav/minimized-nav.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const MinimizedTopNav: React.FC<{}> = () => {
   return (
-    <div className="flex items-center justify-between py-2 px-5 fixed z-10 left-0 right-0 top-0">
+    <div className="flex items-center justify-between py-2 px-5 fixed z-10 left-0 right-0 top-0 bg-black">
       <Image src="/logo-white.svg" width={150} height={150} alt="Logo" />
       <div className="flex items-center gap-5">
         <Image src="/search-white.svg" width={20} height={20} alt="Logo" />
