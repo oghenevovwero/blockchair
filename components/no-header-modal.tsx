6948d3e079cbf5d38b54dc55d9fc154eff8b94fc
oghenevovwerho <oghenevovwerho.iruo@gmail.com>
2024-02-13 13:54:56 +0000

    adjusted modal width

--- components/no-header-modal.tsx
+++ components/no-header-modal.tsx
@@ -69,13 +69,13 @@ export default function Modal({
       <div
         className="
       relative
-       w-full
+       w-[95%]
        md:w-4/6
        lg:w-3/6
        xl:w-2/5
        my-6
        mx-auto
-       h-full
+       h-fit
        lg:h-auto
        md:h-auto
        "
